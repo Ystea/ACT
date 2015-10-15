f5fdd45a6c83d44d2a72e286e3f1f059e38191d6
Haelia <sarahwissocq@free.fr>
2015-10-15 10:00:40 +0200

    debut version dynamique

--- tp3/algo/Game.java
+++ tp3/algo/Game.java
@@ -1,6 +1,7 @@
 package algo;
 
 import java.util.*;
+import algo.*;
 
 /**
  * @author Adrien Agez
@@ -8,10 +9,19 @@ import java.util.*;
  */
 public class Game {
     
-    public static int naif(int m, int n, int x, int y) {
+    private int m;
+    private int n;
+    private int x;
+    private int y;
+    
+    public Game(int m, int n, int x, int y) {
+        m = m; n = n; i = i; j = j;
+    }
+    
+    public static int naif() {
 	List<Integer> positif = new LinkedList<Integer>();
 	List<Integer> negatif = new LinkedList<Integer>();
-	if (m==n && n == 1 && x==y && x==0){
+	if (m==n && n == 1 && x==y && x==0) {
             return 0;
 	}
 	else{
@@ -71,20 +81,63 @@ public class Game {
 	return res;
     }
 
+   
     public static int dynamique(){
-	
+    /** Initialisation des tableaux*/
+	int[][][][] tab = new int[m][n][x][y];
+	boolean[][][][] calc = new boolean[m][n][x][y];
+	/** Remplissage des tableaux*/
+	tab[1][1][0][0]=0;
+	for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < x; k++) 
+                    for (int l = 0; l < y; l++)
+                        calc[i][j][k][l] = false;
+	for (int i = 1; i < m; i++) {
+            tab[i][1][0][0] = 1;
+            calc[i][1][0][0] = true;
+        }
+        for (int j = 1; j < n; j++) {
+            tab[1][j][0][0] = 1;
+            calc[1][j][0][0] = true;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                for (int k = 0; k < i-1; k++) {
+                    for (int l = 0; l < j-1; l++) {
+                        List<Quadruplet> succes = successeur(i, j, k, l);
+                        
+                    }
+                }
+            }
+        }
 	return 0;
+    }
+    
+    
+    public static List<Quadruplet> successeur(int m, int n, int x, int y){
+    List<Quadruplet> res = new ArrayList<Quadruplet>();
+        for (int idx = 1; idx < this.m; idx++) {
+            if (m-idx >=0)                              // Coupure verticale avant le carré
+                res.add(new Quadruplet(m-idx , n, x-idx, y));
+            else                                        // Coupure verticale après le carré
+                res.add(new Quadruplet(idx, n, x, y));
+        }
+        for (int idx = 1; idx < this.n; idx++) {
+            if (n-idx >=0)                              // Coupure horizontale avant le carré
+                res.add(new Quadruplet(m , n-idx, x, y-idx));
+            else                                        // Coupure horizontale après le carré
+                res.add(new Quadruplet(m, idx, i, j));
+            }
     }
 
     public static void main(String[] args) {
-	System.out.println(naif(3,2,2,0));
+	System.out.println(new Game(3,2,2,0).naif());
 	long temps=System.nanoTime();
-	System.out.println(naif(10,7,7,3));
+	System.out.println(new Game(10,7,7,3).naif());
 	System.out.println("naif(10,7,7,3) = " +(System.nanoTime() - temps));
 	temps=System.nanoTime();
-	System.out.println(naif(10,7,5,3));
+	System.out.println(new Game(10,7,5,3).naif());
 	System.out.println("naif(10,7,5,3) = " +(System.nanoTime() - temps));
-	System.out.println(dynamique(100,100,50,50));
-	System.out.println(dynamique(100,100,48,52));
     }
 }
